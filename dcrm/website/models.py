c046ba8e8ce363f5ca7ea403dd4459149ffb214a
hidden bebb <cbj4133@naver.com>
2023-08-09 12:26:15 +0900

    Initial Commit

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    #수정된프로젝트
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 50)
+    city = models.CharField(max_length = 20)
+    phone = models.CharField(max_length = 20)
+    sex = models.CharField(max_length=10)
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Record
 
 # Create your views here.
+
+def home(request):
+    records = Record.objects.all()
+    if request.method =='Post':
+        return render(request, 'home.html',{})
+    else:
+        return render(request, 'home.html',{'records':records})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "로그인 성공")
+            return redirect('home')
+        else:
+            messages.success(request, "아이디 혹은 비밀번호가 틀렸습니다.")
+    return render(request, 'login.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "로그아웃 완료")
+    return redirect('home')
